8049a6aa2f5fa06851dd4ed90b3d9d34e1c49183
731614105005 <bawadharani96@gmail.com>
2017-07-04 17:56:01 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,20 @@
+# set
+import java io.*;
+import java util.*;
+public class Main
+{
+public static void main(String args[])throws IOException
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+int n=sc.nextInt();
+if(n/2!=0)
+{
+System.out.println("n is even");
+}
+else 
+{
+System.out.println("n is odd");
+}
+}
+}
